a876a0ccf95235dd53b9cc470ec20d19fa900ef2
jimin <w2037@e-mirim.hs.kr>
2021-07-22 10:45:57 +0900

    Edit menu2(Dir2_1) Commit

---
+++ app/src/main/java/ks/hs/emirim/wlalsdl_04/project_7_1/MainActivity2.java
@@ -0,0 +1,14 @@
+package ks.hs.emirim.wlalsdl_04.project_7_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
